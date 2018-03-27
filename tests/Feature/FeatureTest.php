d918038ad71c67e4f1083a47ab91a3764d3dd4a8
W01fpaw <wsarchett@comcast.net>
2018-03-26 19:53:22 -0700

    Login Page feature test returns a 200 status code

--- tests/Feature/FeatureTest.php
+++ tests/Feature/FeatureTest.php
@@ -19,4 +19,11 @@ class FeatureTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testLoginPage()
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
 }
